eccd09063073e599e925d94ed4ccf89d2c6e30f6
Anderson Afonso <aafonso56@gmail.com>
2021-11-05 14:37:29 -0300

    Reducers separation

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,17 @@ export default () => {
     const [state, dispatch] = useStateValue();
     return (
         <header className={`box theme-${state.theme}`}>
-            {state.theme === 'light' && <label className='switch'>
-                <input type='checkbox' onChange={()=>dispatch({type: 'setTheme', theme:'dark' })} />
-                <span className='slider'></span>
-            </label> }
-            {state.theme === 'dark' && <label className='switch'>
-                <input type='checkbox' checked onChange={()=>dispatch({type: 'setTheme', theme: 'light'})} />
-                <span className='slider'></span>
-            </label>}
-            
+            {state.theme === 'light' &&
+                <label className='switch'>
+                    <input type='checkbox' onChange={() => dispatch({ type: 'setTheme', theme: 'dark' })} />
+                    <span className='slider'></span>
+                </label>}
+            {state.theme === 'dark' &&
+                <label className='switch'>
+                    <input type='checkbox' checked onChange={() => dispatch({ type: 'setTheme', theme: 'light' })} />
+                    <span className='slider'></span>
+                </label>}
+
         </header>
     );
 }
--- src/contexts/stateContext.js
+++ src/contexts/stateContext.js
@@ -1,40 +1,24 @@
-import {createContext, useContext, useReducer} from 'react';
+import { createContext, useContext, useReducer } from 'react';
 
+import ThemeReducer from '../reducers/ThemeReducer';
+import UserReducer from '../reducers/UserReducer';
 
 const initialState = {
-    theme: 'light',
-    user: {
-        name: 'Anderson Afonso',
-        email: 'aafonso56@gmail.com'
-    }
+    theme: ThemeReducer(),
+    user: UserReducer()
 }
 
-const reducer = (state, action)=>{
-    switch(action.type){
-        case 'setTheme':
-            return {...state, theme: action.theme};
-        break;
-        case 'setName':
-            let newUserName = {...state.user}
-            newUserName.name = action.name
-            return {...state, user: newUserName}
-        break;
-        case 'setEmail':
-            let newUserEmail = {...state.user}
-            newUserEmail.email = action.email
-            return {...state, user: newUserEmail}
-        break;     
-    }
+const MainReducer = (state, action) => ({
+    theme: ThemeReducer(state.theme, action),
+    user: UserReducer(state.user, action)
+});
 
-    return state
-}
+export const StateContext = createContext();
 
-export const StateContext =  createContext();
-
-export const StateProvider = ({children})=>(
-    <StateContext.Provider value={useReducer(reducer, initialState)}>
+export const StateProvider = ({ children }) => (
+    <StateContext.Provider value={useReducer(MainReducer, initialState)}>
         {children}
     </StateContext.Provider>);
 
 
-export const useStateValue = ()=> useContext(StateContext);
+export const useStateValue = () => useContext(StateContext);
