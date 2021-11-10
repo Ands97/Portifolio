6af97791168a6ca8ed4511047b66ff21d9e6014b
Anderson Afonso <aafonso56@gmail.com>
2021-11-10 09:12:30 -0300

    Mobile Css

--- src/components/Body.js
+++ src/components/Body.js
@@ -55,22 +55,6 @@ export default () => {
                             a type specimen book. It has survived not only five centuries,
                             but also the leap into.
                         </p>
-                        <br />
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type
-                            specimen book. It has survived not only five centuries, but also the leap into.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                            Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                            unknown printer took a galley of type and scrambled it to make a type specimen
-                            book. It has survived not only five centuries, but also the leap into.Lorem
-                            Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                            Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make
-                            a type specimen book. It has survived not only five centuries,
-                            but also the leap into.
-                        </p>
                     </main>
                 </article>
             </section>
@@ -104,6 +88,12 @@ export default () => {
                         <a target='blank' href='https://api.whatsapp.com/send?phone=5583986538971&text=Ol%C3%A1!'><FaWhatsapp />   (83) 98653-8971</a>
                         <a target='blank' href='https://www.instagram.com/anderson.afonso.dev/'><FaInstagram />   @anderson.afonso.dev</a>
                         <a target='blank' href='https://github.com/Ands97'><FaGithub />   https://github.com/Ands97</a>
+                    </div>
+                    <div className='profile-mobile'>
+                        <h1>Redes Sociais:</h1><br/>
+                        <a target='blank' href='https://api.whatsapp.com/send?phone=5583986538971&text=Ol%C3%A1!'><FaWhatsapp className='profile-icons' /></a>
+                        <a target='blank' href='https://www.instagram.com/anderson.afonso.dev/'><FaInstagram className='profile-icons' /></a>
+                        <a target='blank' href='https://github.com/Ands97'><FaGithub className='profile-icons' /></a>
                     </div>
                 </div>
             </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import { useStateValue } from '../contexts/stateContext';
-
+import { IoMenu } from "react-icons/io5";
 import Logo from '../images/logo.png'
 
 export default () => {
     const [state, dispatch] = useStateValue();
     return (
         <header >
-           <div className='logo'>
-               <img src={Logo}/>
-           </div>
-           <nav className='menu'>
-               <ul>
-                   <li>Home</li>
-                   <li>Sobre</li>
-                   <li>Projetos</li>
-                   <li className='key'>Contatos</li>
-               </ul>
-           </nav>
+            <div className='logo'>
+                <img src={Logo} />
+            </div>
+            <div className='menu-burguer'>
+                <IoMenu className='ioMenu' />
+            </div>
+            <nav className='menu'>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Projetos</li>
+                    <li className='key'>Contatos</li>
+                </ul>
+            </nav>
         </header>
     );
 }
